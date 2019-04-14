1828f9a02de697f45c687eba3234b83ad032082d
Khang Doan <caokhang91@gmail.com>
2019-04-14 23:47:36 +0700

    remove pkg

--- pwa/pwa.go
+++ pwa/pwa.go
@@ -1,9 +1,16 @@
 package pwa
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
+func reverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+func main() {
 	fmt.Println("Hello PWA!")
 }
